fe405a7dc67478807d7123e69f1aba4431eb6129
Gokul <gokulchandan18@gmail.com>
2022-01-13 08:56:08 +0530

    commit

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -14,6 +14,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
 
 const MaterialComponents=[
   MatMenuModule,
@@ -30,7 +31,8 @@ const MaterialComponents=[
   MatInputModule,
   MatSelectModule,
   MatAutocompleteModule,
-  MatCheckboxModule
+  MatCheckboxModule,
+  MatRadioModule
 ];
 
 @NgModule({
